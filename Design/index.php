9350da74deb2c2934a652a1f4a7b84b2a9c677e6
Sowvick Bose <34067987+laptopworldbd@users.noreply.github.com>
2019-07-28 19:50:56 +0600

    add to design folder

---
+++ Design/index.php
@@ -0,0 +1,177 @@
+<?php
+include("design.php");
+?>
+
+<!DOCTYPE html>
+<html>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karma">
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Karma", sans-serif}
+.w3-bar-block .w3-bar-item {padding:20px}
+</style>
+<body>
+
+
+
+
+  
+
+<div class="w3-main w3-content w3-padding" style="max-width:1200px;margin-top:100px">
+<center>
+  
+  <div class="w3-row-padding w3-padding-16 w3-center" id="food">
+    <div class="w3-quarter">
+      <img src=" img/diu.png" alt="Sandwich" style="background position=cover; width:50%;">
+      <h3>Daffodil International University</h3>
+      <p> 
+       Faculty of Science & Information Technology
+Undergraduate Programs
+▶B.Sc in Computer Science and Engineering (CSE)<br>
+▶B.Sc in Computer Science and Engineering (CSE) (For Diploma Holder)<br>
+▶B.Sc in Computing and Information System (CIS)<br>
+▶B.Sc in Software Engineering (SWE)<br>
+▶B.Sc in Multimedia and Creative Technology (MCT)<br>
+▶B.Sc in Environmental Science and Disaster Mgt. (ESDM)
+      </p>
+    </div>
+    <div class="w3-quarter">
+      <img src="img/BUETt.jpg" alt="Steak" style="width:90%">
+      <h3>Bangladesh University of Engineering and Technology</h3>
+      <p> 
+➽Department of Chemical Engineering (ChE)<br>
+➽Department of Materials and Metallurgical Engineering (MME)<br>
+➽Department of Chemistry (Chem)<br>
+➽Department of Mathematics (Math)<br>
+➽Department of Physics (Phys)<br>
+➽Department of Petroleum and Mineral Resources Engineering (PMRE)<br>
+➽Department of Glass and Ceramic Engineering (GCE)
+
+      </p>
+    </div>
+    <div class="w3-quarter">
+      <img src="img/date.jpg" alt="Cherries" style="width:100%">
+      <h3>All Public University Admission Test Notice Circular 2018-19</h3>
+      <p>
+        
+
+      </p>
+      
+    </div>
+    <div class="w3-quarter">
+      <img src="img/admission4.jpg" alt="Pasta and Wine" style="width:100%">
+      <h3>Admit In A Private University</h3>
+      <p>➢Bangladesh Army University of Science and Technology (BAUST), Saidpur, Admission, Programs and Ranking Bangladesh Army University of Science and Technology (BAUST) is an army funded private university.</p>
+    </div>
+  </div>
+  
+  
+  <div class="w3-row-padding w3-padding-16 w3-center">
+    <div class="w3-quarter">
+      <img src="img/buet.jpg" alt="Popsicle" style="width:100%">
+      <h3>Bangladesh University of Engineering and Technology(BUET)</h3>
+      <p>All Adimission Circular</p>
+    </div>
+    <div class="w3-quarter">
+      <img src="img/nsu.jpg" alt="Salmon" style="width:30%">
+      <h3>North South University</h3>
+      <p>Bachelor of Architecture<br>
+      ➻B.Sc. Engg. in Civil<br>
+      ➻B.Sc. Engg. in CSE<br>
+      ➻B.Sc. Engg. in EEE<br>
+      ➻Bachelor of Business Administration<br>
+      ➻LL.B. (Hon's)</p>
+    </div>
+    <div class="w3-quarter">
+      <img src="img/uap.png" alt="Sandwich" style="width:30%">
+      <h3>University Of Asia Pasific</h3>
+      <p>Admission tests are held thrice a year for Spring, Summer and Fall semesters.</p>
+    </div>
+    <div class="w3-quarter">
+      <img src="img/7college.jpg" alt="Croissant" style="width:100%">
+      <h3>সাতটি কলেজ </h3>
+      <p>
+      ➥ঢাকা কলেজ<br>
+      ➥কবি নজরুল সরকারি কলেজ<br>
+      ➥ইডেন মহিলা কলেজ<br>
+      ➥বেগম বদরুন্নেছা সরকারি মহিলা কলেজ<br>
+      ➥সরকারি শহীদ সোহরাওয়ার্দী কলেজ<br>
+      ➥সরকারি বাঙলা কলেজ ও সরকারি তিতুমীর কলেজ</p>
+    </div>
+  </div>
+
+  
+  <div class="w3-center w3-padding-32">
+    <div class="w3-bar">
+      <a href="#" class="w3-bar-item w3-button w3-hover-black">«</a>
+      <a href="#" class="w3-bar-item w3-black w3-button">1</a>
+      <a href="#" class="w3-bar-item w3-button w3-hover-black">2</a>
+      <a href="#" class="w3-bar-item w3-button w3-hover-black">3</a>
+      <a href="#" class="w3-bar-item w3-button w3-hover-black">4</a>
+      <a href="#" class="w3-bar-item w3-button w3-hover-black">»</a>
+    </div>
+  </div>
+  
+  <hr id="about">
+
+  
+  <div class="w3-container w3-padding-32 w3-center">  
+    
+    <img src="img/admission2.jpg" alt="Me" class="w3-image" style="display:block;margin:auto" width="1000" height="533">
+    <div class="w3-padding-32">
+      <h4><b> </b></h4>
+      <h6><i>Admin In A Public University </i></h6>
+      <p>There are 34 Public University in Bangladesh and Private University has more than 65. Every Public University take admission test for the honours program. You will get all public university admission test Circular information for academic session 2019-20 Bangladesh from our website.</p>
+    </div>
+  </div>
+  <hr>
+  
+  
+  <footer class="w3-row-padding w3-padding-32">
+   
+    <div class="w3-third">
+      <h3>For Any Querry</h3>
+      <ul class="w3-ul w3-hoverable">
+        <li class="w3-padding-16">
+          <img src="img/loc.png" class="w3-left w3-margin-right" style="width:50px">
+          <span class="w3-large">Office</span><br>
+          <span>Sukrabad, Dhanmondi32, Dhaka1209</span>
+        </li>
+        <li class="w3-padding-16">
+          <img src="img/mail.jpg" class="w3-left w3-margin-right" style="width:50px">
+          <span class="w3-large">Mail@</span><br>
+          <span>sowvick35-1562@diu.edu.bd</span>
+        </li> 
+        <li class="w3-padding-16">
+          <img src="img/fb.jpg" class="w3-left w3-margin-right" style="width:50px">
+          <span class="w3-large">Facebook</span><br>
+          <span>http//:facebook.admisssionhelper.com</span>
+        </li> 
+      </ul>
+    </div>
+
+    <div class="w3-third w3-serif">
+     
+    </div>
+  </footer>
+</center>
+
+</div>
+
+<script>
+
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+ 
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+</script>
+
+</body>
+</html>
+
